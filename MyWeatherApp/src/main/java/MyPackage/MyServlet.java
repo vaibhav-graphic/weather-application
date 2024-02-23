6f071bad30517b3547c856909a04715d9326709c
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2024-02-23 16:34:37 +0530

    Web application in java

---
+++ MyWeatherApp/src/main/java/MyPackage/MyServlet.java
@@ -0,0 +1,97 @@
+package MyPackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.naming.ContextNotEmptyException;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public MyServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String api = "ae4af9c7d7c651552ee6d2eef5cb890d"; // api hai
+		String city = request.getParameter("city");
+		String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api;
+
+		// url integration
+		URL url = new URL(apiUrl);
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		connection.setRequestMethod("GET");
+
+		// reading the data from network
+		InputStream inputStream = connection.getInputStream();
+		InputStreamReader reader = new InputStreamReader(inputStream);
+
+		StringBuilder responseContent = new StringBuilder();
+
+		// take data from reader
+		Scanner scanner = new Scanner(reader);
+
+		while (scanner.hasNext()) {
+			responseContent.append(scanner.nextLine());
+		}
+
+		scanner.close();
+
+//		System.out.println(responseContent);
+
+		Gson gson = new Gson();
+		JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+
+//		System.out.println(jsonObject);
+
+		// date and time
+		long dateTimestamp = jsonObject.get("dt").getAsLong() * 1000;
+		String date = new Date(dateTimestamp).toString();
+
+		// Temperature
+		double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+		int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+
+		// Humidity
+		int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+
+		// Wind Speed
+		double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+
+		// Weather Condition
+		String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+		
+		request.setAttribute("date", date);
+		request.setAttribute("city", city);
+		request.setAttribute("temperture", temperatureCelsius);
+		request.setAttribute("humidity", humidity);
+		request.setAttribute("windSpeed", windSpeed);
+		request.setAttribute("weatherData", responseContent.toString());
+		
+		connection.disconnect();
+		
+		request.getRequestDispatcher("index.jsp").forward(request, response);;
+		
+	}
+
+}
